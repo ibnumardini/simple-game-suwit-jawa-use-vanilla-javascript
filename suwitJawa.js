67869b4610f766dc75b2b4b13530ccfb259e2249
kurstudio <fatkurozy.muhammad28@gmail.com>
2019-10-30 09:35:30 +0700

    Add computer choice.

--- suwitJawa.js
+++ suwitJawa.js
@@ -1,3 +1,17 @@
 // Menangkap Pilihan Player
 
 var pilih = prompt('pilih : gajah, semut, atau orang!')
+
+// Mengangkap pilihan Komputer
+
+var komputer = Math.random()
+
+if (komputer < 0.34) {
+  komputer = 'gajah'
+}else if(komputer >= 0.34 && komputer < 0.67){
+  komputer = 'orang'
+}else{
+  komputer = 'orang'
+}
+
+console.log(komputer)
